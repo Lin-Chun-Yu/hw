bd5d065a0395ff349b059624b7d27a93cbdc4d0c
lin_chun_yu <lin.chun.yu.0405@gmail.com>
2021-06-25 22:20:32 +0800

    First commit

---
+++ YouTube/playList.js
@@ -0,0 +1,12 @@
+//YouTube Video ID
+let playList = [
+    "SwZMS-ISbH4",
+    "c6gi6IP6ay4",
+    "SwZMS-ISbH4",
+    ];
+    //播放起訖秒數
+    let playTime = [
+    [101,109],
+    [144,150],
+    [146,152]
+    ];
